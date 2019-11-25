251955776998e0e341f38b5ace8317ddccdcb7d2
Armand Nasseri <aanasseri@ucdavis.edu>
2019-11-24 16:09:49 -0800

    problem 1 almost done

--- HW3/problem1.py
+++ HW3/problem1.py
@@ -7,10 +7,80 @@ Created on Wed Nov 13 10:20:25 2019
 """
 
 import numpy as np
+from sklearn import datasets
+from sklearn import linear_model
+from sklearn.model_selection import GridSearchCV
 import pandas as pd
 import warnings
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+np.random.seed(0)
 warnings.filterwarnings("ignore")
 
+# lambda 0 - 1 try 10 values
 # Import the dataset 
-dataset = pd.read_csv('ecs171.dataset.txt', delim_whitespace=True)
-# X features 
+dataset = pd.read_csv('ecs171.dataset.txt', delim_whitespace=True).dropna()
+
+# X features
+X = dataset.iloc[:,6:4503]
+# Dropping the last column to avoid type error
+X = X.iloc[:, :-1]
+
+
+# y prediction
+y = dataset['GrowthRate']
+
+#Set the different values of alpha to be tested
+alpha_ridge = np.array([1e-15, 1e-10, 1e-8, 1e-4, 1e-3,1e-2, 1, 5, 10, 20])
+
+X_train,X_test,y_train,y_test = train_test_split(X,y, test_size=0.3, random_state=31)
+
+
+lasso = linear_model.Lasso()
+grid = GridSearchCV(estimator=lasso, scoring = "neg_mean_squared_error",param_grid = dict(alpha=alpha_ridge), cv = 5)
+grid.fit(X,y)
+
+
+
+lassoBest = linear_model.Lasso(alpha=grid.best_estimator_.alpha, max_iter=10e5)
+lassoBest.fit(X,y)
+
+coeff_used = np.sum(lassoBest.coef_!=0)
+
+print("The 5-fold cross-validation error is:",-grid.best_score_)
+print("The optimal constrained lambda parameter value is:",grid.best_estimator_.alpha)
+print("The number of features that have non-zero coefficients for alpha = 0.0001 is:", coeff_used)
+
+#means = grid.cv_results_['mean_test_score']
+#stds = grid.cv_results_['std_test_score']
+#params = grid.cv_results_['params']
+#for mean, stdev, param in zip(means, stds, params):
+#    print("%f (%f) with: %r" % (mean, stdev, param))
+
+#it = 0
+#for i in alpha_ridge:
+ #   lasso = linear_model.Lasso(alpha=i, max_iter=10e5)
+  #  lasso.fit(X_train,y_train)
+   # print("Iteration:", it)
+    #print("")
+    #it = it+1
+
+    #train_score=lasso.score(X_train,y_train)
+    #test_score=lasso.score(X_test,y_test)
+    #coeff_used = np.sum(lasso.coef_!=0)
+
+    #print("training score:", train_score) 
+    #print("test score: ", test_score)
+    #print("number of features used: ", coeff_used)
+
+#print(cross_val_score(lasso, X, y, cv=5))
+
+
+
+
+        
+        
+
+
+
